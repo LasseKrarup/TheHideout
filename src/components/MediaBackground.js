bf5150d3de0459765f2a75f413efd707126cd44b
Lasse Herold Krarup <latze95@gmail.com>
2020-12-09 08:36:56 +0100

    Hopefully fixed responsive video and fixed viewport width error (scrollbar)

--- src/components/MediaBackground.js
+++ src/components/MediaBackground.js
@@ -1,10 +1,16 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import PropTypes from 'prop-types';
 import { Img } from "gatsby-image"
 
 const VideoHelper = ({src}) => {
+    const videoRef = useRef(null)
+
+    useEffect(() => {
+        const { current: videoElement } = videoRef
+        videoElement.setAttribute('muted', '')
+    }, [])
     return (
-        <video autoPlay={true} muted="true" loop="loop" className="absolute inset-0 m-auto max-w-none h-screen overflow-hidden" style={{minWidth: "50%", minHeight: "50%"}}>
+        <video ref={videoRef} autoPlay playsInline muted loop className="absolute inset-0 m-auto max-w-none h-screen overflow-hidden" style={{minWidth: "50%", minHeight: "50%"}}>
             <source src={src} type="video/mp4" />
             Browser doesn't support the video tag.
         </video>
@@ -29,7 +35,7 @@ const MediaBackground = ({src}) => {
     const [srcType] = useState(typeof src)
 
     return (
-        <div className="overflow-hidden absolute inset-0 w-screen h-screen" style={{zIndex: -10}}>
+        <div className="overflow-hidden absolute inset-0 w-screen h-screen max-w-full" style={{zIndex: -10}}>
             <div className="absolute -left-1/2 -top-1/2 mx-auto overflow-hidden" style={{width: "200%", height: "200%"}}>
                 {srcType === "string" ? <VideoHelper src={src} /> : <ImageHelper src={src} />}
             </div>
