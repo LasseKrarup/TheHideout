2b7dabbe4cf25276a93b672dda444b8aaca2bb03
Lasse Herold Krarup <latze95@gmail.com>
2020-12-09 10:40:56 +0100

    Added forward ref to Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, {forwardRef} from 'react';
 
 import { navigation } from "../js/navigation.js"
 import { v4 as uuid } from "uuid"
 import NavlinkToplevel from './util/NavlinkToplevel.js';
 
-const Nav = () => {
+const Nav = forwardRef((props, ref) => {
 
     return (
-        <nav className="flex font-heading lowercase">
+        <nav ref={ref} className="flex font-heading lowercase">
             <ul className="flex flex-col text-2xl ml-8 mt-8">
                 {navigation.map(item => (
                     <NavlinkToplevel key={uuid()} title={item.title} slug={item.slug} sub={item.sub} />
@@ -15,6 +15,6 @@ const Nav = () => {
             </ul>
         </nav>
     );
-}
+})
 
 export default Nav;
