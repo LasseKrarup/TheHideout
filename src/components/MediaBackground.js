02dd70542aaeb91445584ad2cba7904b234ddfdb
Lasse Herold Krarup <latze95@gmail.com>
2020-12-10 15:28:31 +0100

    Trying to fix autoplay on mobile

--- src/components/MediaBackground.js
+++ src/components/MediaBackground.js
@@ -7,10 +7,11 @@ const VideoHelper = ({src}) => {
 
     useEffect(() => {
         const { current: videoElement } = videoRef
-        videoElement.setAttribute('muted', '')
+        videoElement.defaultMuted = true
+        // videoElement.muted = true
     }, [])
     return (
-        <video ref={videoRef} autoPlay playsInline muted loop className="absolute inset-0 m-auto max-w-none overflow-hidden" style={{minWidth: "50%", minHeight: "50%"}}>
+        <video ref={videoRef} muted autoPlay playsInline loop className="absolute inset-0 m-auto max-w-none overflow-hidden" style={{minWidth: "50%", minHeight: "50%"}}>
             <source src={src} type="video/mp4" />
             Browser doesn't support the video tag.
         </video>
