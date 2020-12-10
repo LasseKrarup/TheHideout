debba624dd4b5d5dbc7c4436ea16eb8ce40c88fb
Lasse Herold Krarup <latze95@gmail.com>
2020-12-10 15:55:12 +0100

    Responsive video again

--- src/components/MediaBackground.js
+++ src/components/MediaBackground.js
@@ -21,6 +21,28 @@ VideoHelper.propTypes = {
     src: PropTypes.string.isRequired
 }
 
+const VideoComponent = ({src}) => {
+    const videoContainer = useRef()
+
+    useEffect(() => {
+        const video = document.createElement('video');
+        video.autoplay = true;
+        video.loop = true;
+        video.muted = true; // fixes autoplay in chrome
+        video.setAttribute('playsinline', 'true'); // fixes autoplay in webkit (ie. mobile safari)
+
+        const source = document.createElement('source');
+        source.src = src;
+        source.type = 'video/mp4';
+        video.appendChild(source);
+
+        videoContainer.current.appendChild(video);
+    },[])
+    return (
+      <div ref={videoContainer} />
+    );
+}
+
 
 const ImageHelper = ({src}) => {
     console.log("Image helper rendered")
@@ -38,7 +60,7 @@ const MediaBackground = ({src}) => {
     return (
         <div className="overflow-hidden absolute inset-0 w-screen h-screen max-w-full">
             <div className="absolute -left-1/2 -top-1/2 mx-auto overflow-hidden" style={{width: "200%", height: "200%"}}>
-                {srcType === "string" ? <VideoHelper src={src} /> : <ImageHelper src={src} />}
+                {srcType === "string" ? <VideoComponent src={src} /> : <ImageHelper src={src} />}
             </div>
             <div className="absolute inset-0 bg-black bg-opacity-75 overflow-hidden" />
         </div>
