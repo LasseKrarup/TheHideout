022417cdafebb66628250f3b620cc390a51d09b2
Lasse Herold Krarup <latze95@gmail.com>
2020-12-14 11:46:51 +0100

    Required fields on form

--- src/pages/about/contact.js
+++ src/pages/about/contact.js
@@ -28,12 +28,12 @@ const ContactForm = () => {
   return(
     <div className="flex flex-col items-center overflow-hidden mb-8">
       <form method="post" action="/success" netlify-honeypot="bot-field" data-netlify="true" name="contact-form" className="text-white bg-transparent flex flex-col">
-        <span className="flex items-center"><Label htmlFor="name">name</Label><Input type="text" name="name" placeholder={`${width >= 640 ? "WRITE YOUR " : ""}NAME HERE`} /></span>
-        <span className="flex items-center"><Label htmlFor="email">email</Label><Input type="email" name="email" pattern=".+@.*\..+" placeholder={`${width >= 640 ? "WRITE YOUR " : ""}EMAIL HERE`} /></span>
-        <span className="flex items-center"><Label htmlFor="phone">phone#</Label><Input type="text" name="phone" pattern="^\+?[0-9]+$" placeholder={`${width >= 640 ? "WRITE YOUR " : ""}PHONE# HERE`} /></span>
+        <span className="flex items-center"><Label htmlFor="name">name</Label><Input type="text" name="name" placeholder={`${width >= 640 ? "WRITE YOUR " : ""}NAME HERE`} required /></span>
+        <span className="flex items-center"><Label htmlFor="email">email</Label><Input type="email" name="email" pattern=".+@.*\..+" placeholder={`${width >= 640 ? "WRITE YOUR " : ""}EMAIL HERE`} required/></span>
+        <span className="flex items-center"><Label htmlFor="phone">phone#</Label><Input type="text" name="phone" placeholder={`${width >= 640 ? "WRITE YOUR " : ""}PHONE# HERE`} /></span>
         <textarea className="text-gray-400 mt-2 bg-black border border-gray-600 rounded-md p-2 focus:outline-none focus:border-white" name="message" placeholder="WRITE YOUR MESSAGE HERE" rows="6"></textarea>
         <input type="hidden" name="form-name" value="contact-form" />
-        <button type="submit" className="rounded-md font-heading lowercase text-lg border border-white p-2 my-2 mr-auto">Send</button>
+        <button type="submit" className="rounded-md font-heading lowercase text-lg border border-white p-2 my-2 mr-auto hover:bg-white hover:text-black">Send</button>
       </form>
     </div>
   )
